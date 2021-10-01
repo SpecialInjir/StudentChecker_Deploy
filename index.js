491eac85277e5961e212099eb251253d7c727e13
Nasiba Uzdenova <uzdennasub@gmail.com>
2021-10-01 20:39:14 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const port = process.env.port | 4000
+
+
+
+app.get("/",(req, res)=>{
+    res.end("<div>Happe me</div>")
+})
+
+app.listen(port, ()=>{
+    console.log("Server has been started!!!!")
+})
